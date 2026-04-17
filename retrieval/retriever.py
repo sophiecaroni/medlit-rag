be2af5da00daaf42d6a93aae82e701baa2a270a3
sophiecaroni <sophie.caroni@gmail.com>
2026-04-17 11:57:13 +0200

    Include similarity score and index position to neighbors information

--- retrieval/retriever.py
+++ retrieval/retriever.py
@@ -7,10 +7,10 @@ from retrieval.vector_store import MedLitRagIndex
 def retrieve_neighbors(query: str, fast: bool = False) -> list[list[dict]]:
     """
     Performs nearest-neighbor search over an existing index to find elements similar to the input query, and returns
-    each neighbors metadata.
+    each neighbors information (metadata, similarity score, index position).
     :param query: User's input query.
     :param fast: If True, loads small version of index (for faster iteration).
-    :return: List of metadata of vectors similar to the input query.
+    :return: Lists of neighbors information per query chunk.
     """
     # Chunk query text and embed them
     query_chunks = chunk_text(query)
@@ -23,8 +23,18 @@ def retrieve_neighbors(query: str, fast: bool = False) -> list[list[dict]]:
     index.load(idx_fname=idx_fname, metadata_fname=metadata_fname)
 
     # Find k most similar neighbors of each query's embedding
-    _, neigh_idxs = index.search(embeddings, k=20)
-
-    # Retrieve and return neighbors metadata
+    neigh_scores, neigh_idxs = index.search(embeddings, k=20)
     neigh_idxs = neigh_idxs.tolist()  # convert to list
-    return [[index.metadata[neigh] for neigh in query_chunk_neighbors] for query_chunk_neighbors in neigh_idxs]
+    neigh_scores = neigh_scores.tolist()  # convert to list
+
+    # Create result list: one list of neighbor information dicts per query chunk
+    neigh_info = []
+    for query_chunk_neigh_ids, query_chunk_neigh_scores in zip(neigh_idxs, neigh_scores):
+        neigh_info.append([])
+        for neigh, score in zip(query_chunk_neigh_ids, query_chunk_neigh_scores):
+            neigh_info[-1].append(dict(  # the current query chunk list is always the last created
+                **index.metadata[neigh],
+                score=score,  # add similarity score to neighbor dict
+                faiss_idx=neigh,  # add position in FAISS to neighbor dict
+            ))
+    return neigh_info
