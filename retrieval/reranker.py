f90eebc280ae72aad6dc3a3659dfa93ad8121748
sophiecaroni <sophie.caroni@gmail.com>
2026-04-17 15:35:06 +0200

    Add reranker.py to rerank query's neighbors using cross-encoder

---
+++ retrieval/reranker.py
@@ -0,0 +1,36 @@
+from sentence_transformers import CrossEncoder
+from copy import deepcopy
+
+model = CrossEncoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
+
+
+def rerank_neighbors(
+        neighbors_information: list[list[dict]],
+        query_chunks: list[str],
+        top_k: int = 5,
+) -> list[list[dict]]:
+    """
+    Re-rank neighbors of a query chunks using a cross-encoder and only return top-k similar neighbors.
+    :param neighbors_information: Lists of neighbors information per query chunk.
+    :param query_chunks: List of chunks of a query.
+    :param top_k: Number of neighbors to return
+    :return: Lists of top-k neighbors information (with updated scores) per query chunk.
+    """
+
+    # Initialise reranked result - use deep copy to avoid mutating the input internal lists
+    reranked = deepcopy(neighbors_information)
+    for chunk_i, (chunk, neighbors) in enumerate(zip(query_chunks, neighbors_information)):
+        pairs = [[chunk, neighbor['chunk_text']] for neighbor in neighbors]
+        new_scores = model.predict(pairs)
+
+        # Update score
+        for neigh_i, chunk_dict in enumerate(reranked[chunk_i]):
+            chunk_dict['score'] = new_scores[neigh_i]
+
+        # Sort neighbors based on new scores and only keep top-k ones
+        reranked[chunk_i].sort(key=lambda neighbor: neighbor['score'], reverse=True)
+        reranked[chunk_i] = reranked[chunk_i][:top_k]
+
+    return reranked
+
+
--- scripts/query.py
+++ scripts/query.py
@@ -1,11 +1,13 @@
 from ingestion.chunker import chunk_text
 from retrieval.retriever import retrieve_neighbors
+from retrieval.reranker import rerank_neighbors
 
 
 def process_query(fast: bool,):
     query = 'What are the latest neuroprosthetic news?' if fast else input('Please enter here your query. ')
     query_chunks = chunk_text(query)
     neighbors_info = retrieve_neighbors(query_chunks, fast=fast)
+    neighbors_reranked = rerank_neighbors(neighbors_info, query_chunks)
 
 
 if __name__ == "__main__":
